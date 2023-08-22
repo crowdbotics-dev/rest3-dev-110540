9bb10774753d64dcaa133709cda8aec284e8124e
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-22 18:19:53 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Swee
+admin.site.register(Swee)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Swee(models.Model):
+    'Generated Model'
+    yruru = models.BigIntegerField()
+    cddb = models.BigIntegerField()
